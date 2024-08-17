1b0d8d0013d032e11d1123f78ee704f59211a673
Amalina <azamalina@maxis.com.my>
2024-08-17 11:02:34 +0800

    add module import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,39 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { getStorage } from 'firebase/storage';
+import { provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  provideFirebaseApp(() => initializeApp(firebaseConfig)),
+  provideFirestore(() => getFirestore()),
+  provideAuth(() => getAuth()),
+  provideStorage(() => getStorage())
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+
+export class AppModule { }
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyClohHqqCNMK3A_sY-RpF48Z9ZIpfdj5hs",
+  authDomain: "ionic-app-2b94b.firebaseapp.com",
+  projectId: "ionic-app-2b94b",
+  storageBucket: "ionic-app-2b94b.appspot.com",
+  messagingSenderId: "942549969568",
+  appId: "1:942549969568:web:64221b82f991896f8f37c5",
+  measurementId: "G-H3QLJLES1S"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
